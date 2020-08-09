252990a1e8f961d8859e1d5a72ea4903f2f2d28d
Khargol <josephklein3@gmail.com>
2020-08-09 17:27:08 -0400

    bug fix

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -22,7 +22,7 @@ class generateHTML {
             <!-- Bootstrap CSS -->
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         
-            <title>Hello, world!</title>
+            <title>My Team Profile</title>
           </head>
           <body style="margin: 0; padding: 0;">
 
